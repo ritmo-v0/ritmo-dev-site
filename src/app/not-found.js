7d8a6492d62c7033cb72ee861c6522c9301456bb
Ritmo Hung <jajh102098@gmail.com>
2024-10-23 04:14:59 +0800

    Chores: not-found.js

---
+++ src/app/not-found.js
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+// Components & UI
+import { WrapperLayout } from "@/components/common/layouts";
+import { Button } from "@/components/ui/button";
+
+
+
+export default function NotFound() {
+	return (
+		<WrapperLayout className="justify-items-center content-center gap-8 h-[calc(100svh_-_3.5rem)]">
+			<p className="font-bold text-5xl">畫一隻雞不存在</p>
+			<Button asChild>
+				<Link href="/" asChild>回到首頁</Link>
+			</Button>
+		</WrapperLayout>
+	);
+}
