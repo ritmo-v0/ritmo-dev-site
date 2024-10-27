fd04411e88cdb6e1c1a462b75db63c9970d7b677
Ritmo Hung <jajh102098@gmail.com>
2024-10-27 16:34:24 +0800

    Chores: Update sitemap

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,15 +1,30 @@
+import { getBaseUrl } from "@/lib/utils";
+
+
+
 export default function sitemap() {
+	const baseUrl = getBaseUrl().href;
+
 	return [
-		// {
-		// 	url: "",
-		// 	lastModified?: new Date(),
-		// 	changeFrequency?: "monthly",
-		// 	priority?: 1,
-		// 	alternates?: {
-		// 		languages?: {
-		// 			//
-		// 		}
-		// 	}
-		// }
+		// # Homepage
+		{
+			url: `${baseUrl}/`,
+			lastModified: new Date("2024-10-19"),
+			changeFrequency: "monthly",
+			priority: 1,
+		},
+		// # tools/*
+		{
+			url: `${baseUrl}/tools`,
+			lastModified: new Date("2024-10-23"),
+			changeFrequency: "monthly",
+			priority: 0.6,
+		},
+		{
+			url: `${baseUrl}/tools/emojis`,
+			lastModified: new Date("2024-10-23"),
+			changeFrequency: "monthly",
+			priority: 0.9,
+		},
 	];
 }
