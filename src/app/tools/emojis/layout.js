6f17001c894880763268364aafb871dde3799742
Ritmo Hung <jajh102098@gmail.com>
2024-10-27 20:40:06 +0800

    Chores: Update /tools/emojis keywords

--- src/app/tools/emojis/layout.js
+++ src/app/tools/emojis/layout.js
@@ -4,7 +4,38 @@ import { generateLocalMetadata } from "@/lib/utils";
 const title = "Emoji 選擇器";
 const description = "輕鬆複製、編輯、預覽 emoji，支援 Twemoji 字體和膚色篩選。";
 const url = "/tools/emojis";
-export const metadata = generateLocalMetadata({ title, description, url });
+const keywords = [
+	// Main Keywords
+	"emoji選擇器",
+	"emoji小工具",
+	"emoji工具",
+	"表情符號 工具",
+	"emoji大全",
+	"表情符號大全",
+	"emoji組合",
+	"表情符號組合",
+	"表符組合",
+	"emoji列表",
+	"emoji tool",
+	"emoji picker",
+	"emoji list",
+
+	// Behavior Keywords
+	"emoji複製",
+	"表情符號複製",
+	"表符複製",
+	"emoji copy paste",
+	"emoji copy and paste",
+
+	// Synonyms
+	"表符",
+	"表情符號",
+	"twemoji",
+
+	// Additional
+	"小工具",
+];
+export const metadata = generateLocalMetadata({ title, description, url, keywords });
 
 
 
