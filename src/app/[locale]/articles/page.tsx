2f96e2c5dfbecafdb537c3e38f579c196f86f477
Ritmo <jajh102098@gmail.com>
2026-05-19 16:25:32 +0800

    Feat: Articles page `ItemList` JSON-LD

--- src/app/[locale]/articles/page.tsx
+++ src/app/[locale]/articles/page.tsx
@@ -1,9 +1,14 @@
 import { tryCatch } from "@/lib/try-catch";
 import { getArticles } from "@/lib/article/utils";
+import { getBaseUrl } from "@/lib/utils";
 
 // Components & UI
 import { ArticleList } from "@/components/main/articles/list";
 import { Code, Wrapper } from "@/components/common/typography";
+import { JsonLd } from "@/components/common/json-ld";
+
+// Types & Interfaces
+import type { ItemList } from "schema-dts";
 
 
 
@@ -11,8 +16,21 @@ export default async function ArticlesPage() {
 	const { data, error } = await tryCatch(getArticles());
 	const articles = data || [];
 
+	const ARTICLES_JSONLD: ItemList = {
+		"@type": "ItemList",
+		name: "文章列表",  // TODO: i18n?
+		url: `${getBaseUrl().origin}/articles`,
+		itemListElement: articles.map((article, index) => ({
+			"@type": "ListItem",
+			position: index + 1,
+			name: article.title,
+			url: `${getBaseUrl().origin}/articles/${article.shortId}`,
+		})),
+	};
+
 	return (
 		<Wrapper className="pt-8 lg:pt-24" width={720}>
+			<JsonLd data={ARTICLES_JSONLD} />
 			{error
 				? <Code>{error.message}</Code>
 				: <ArticleList articles={articles} />}
