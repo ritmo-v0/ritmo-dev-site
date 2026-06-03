89c70f3281527feb79a36cac8d711d753f2cbd51
Ritmo <jajh102098@gmail.com>
2026-06-03 18:40:10 +0800

    Temp: Remove article TOC

--- src/app/[locale]/articles/[articleId]/page.tsx
+++ src/app/[locale]/articles/[articleId]/page.tsx
@@ -4,7 +4,6 @@ import { getArticle } from "@/lib/article/utils";
 import { getBaseUrl } from "@/lib/utils";
 
 // Components & UI
-import { ArticleTOC } from "@/components/main/articles/toc";
 import { ArticleHeader } from "@/components/main/articles/header";
 import { ArticleContent } from "@/components/main/articles/content";
 import { Wrapper } from "@/components/common/typography";
@@ -45,21 +44,12 @@ export default async function ArticlePage(
 	};
 
 	return (
-		<Wrapper
-			className="grid grid-cols-4 gap-12 max-lg:[--max-width:720px]!"
-			width={976}
-		>
+		<Wrapper className="grid grid-cols-1 gap-12" width={720}>
 			<JsonLd data={ARTICLE_JSONLD} />
-			<div className="col-span-full lg:col-span-3">
-				<ArticleHeader metadata={article.metadata} />
-				<main className="mt-12">
-					<ArticleContent content={article.content} />
-				</main>
-			</div>
-			<ArticleTOC
-				className="max-lg:hidden"
-				content={article.content}
-			/>
+			<ArticleHeader metadata={article.metadata} />
+			<main>
+				<ArticleContent content={article.content} />
+			</main>
 		</Wrapper>
 	);
 }
